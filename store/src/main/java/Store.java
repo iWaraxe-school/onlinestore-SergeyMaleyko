e169823128c7af5c9afa9d71dcd55728cc81fb11
iWaraxe <80040226+iWaraxe@users.noreply.github.com>
2022-04-26 11:16:30 +0300

    Update Store.java

--- store/src/main/java/Store.java
+++ store/src/main/java/Store.java
@@ -1,3 +1,37 @@
+package by.issoft.store;
+
+import by.issoft.domain.Category;
+import by.issoft.domain.Product;
+
+import java.util.*;
+
 public class Store {
-    public Store() {}
+
+    public List<Category> categoryList = new ArrayList<>();
+    protected List<Product> productList = new ArrayList<>();
+
+    private static Store storeInstance;
+
+    private Store() {
+    }
+
+    public static Store getInstance() {
+        if (storeInstance != null) {
+            return storeInstance;
+        }
+        synchronized (Store.class) {
+            if (storeInstance == null) {
+                storeInstance = new Store();
+            }
+            return storeInstance;
+        }
+    }
+
+    public List<Category> getCategoryList() {
+        return categoryList;
+    }
+
+    public List<Product> getProductList() {
+        return productList;
+    }
 }
